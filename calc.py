00448bb696e720b640a42fa7b15dcdc964887c9b
Deepak <chintadadeepak31@gmail.com>
2023-09-28 17:55:11 +0530

    done upto self,constructor and compare

---
+++ calc.py
@@ -0,0 +1,13 @@
+def add():
+    print('in add')
+
+def sub():
+    print('in sub')
+
+def main():
+    print('Hello World')
+    print('Welcome User')
+
+if __name__ == "__main__":
+    main()
+# print(__name__)
