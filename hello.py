61b22d6df67988d310708146919396c5bb334898
Deepak <chintadadeepak31@gmail.com>
2023-09-30 16:52:52 +0530

    done upto exception handling

--- hello.py
+++
@@ -1,9 +0,0 @@
-# polymorphism 
-# poly --> many
-# morphism --> beahviour
-# polymorphism is implemented in many ways
-# 1. Duck Typing
-# 2. Operators overloading
-# 3. Method overloading 
-# 4. Method overriding
- 
