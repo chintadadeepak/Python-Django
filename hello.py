33fcb3e3ba30eabe493d409f850ff94c03d5dc3b
Deepak <chintadadeepak31@gmail.com>
2023-09-30 12:45:17 +0530

    done upto operators overloading

---
+++ hello.py
@@ -0,0 +1,9 @@
+# polymorphism 
+# poly --> many
+# morphism --> beahviour
+# polymorphism is implemented in many ways
+# 1. Duck Typing
+# 2. Operators overloading
+# 3. Method overloading 
+# 4. Method overriding
+ 
